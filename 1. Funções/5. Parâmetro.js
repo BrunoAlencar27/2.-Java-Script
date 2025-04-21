5dc4d264c4b70abc5e82fb3e27bc4dff2e55ad16
BrunoAlencar27 <Bruno.alencar.2017@gmail.com>
2025-04-20 22:45:22 -0300

    s

---
+++ 1. Funções/5. Parâmetro.js
@@ -0,0 +1,8 @@
+//Preparando função para receber argumento
+function exemplo(parametro1, parametro2){
+    console.log("Recebi o valor",parametro1);
+    console.log("Recebi o valor",parametro2);
+}
+
+//Passando argumento para a função
+exemplo(1,2);
---
+++ 1. Funções/6.arguments.js
@@ -0,0 +1,5 @@
+function exemplo(){
+    console.log(arguments);
+}
+
+exemplo(1,2,3,4);
