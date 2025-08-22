9b0594863099fe03439c0a9abed3caac94cda3f8
BrunoAlencar27 <Bruno.alencar.2017@gmail.com>
2025-08-22 16:38:34 -0300

    sobre

---
+++ 8. Tooling e ES6 Modules/3. Modules(Import - Export)/src/index.js
@@ -0,0 +1,4 @@
+//importação - exportação nomeada por lita
+import {nome,sobrenome,soma} from "./modulo 1.js"
+console.log(nome,sobrenome);
+console.log(soma());
---
+++ 8. Tooling e ES6 Modules/3. Modules(Import - Export)/src/modulo 1.js
@@ -0,0 +1,10 @@
+const nome = "Bruno";
+const sobrenome = "Alencar";
+
+function soma (a,b){
+    return a+b;
+}
+
+
+//EXPORTAÇÃO NOMEADA POR LISTA
+export {nome,sobrenome,soma};
